733c560a6d27b332655ea086b4b400b6812106ba
Udit Gandhi <coder.udit@gmail.com>
2022-07-20 21:05:58 -0300

    Added cloud function for processing orders.

---
+++ backend/kitchen-service/cloudFuncs/processOrder.js
@@ -0,0 +1,33 @@
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+exports.helloPubSub = (event, context) => {
+  const message = event.data
+    ? Buffer.from(event.data, "base64").toString()
+    : "Hello, World";
+  console.log(message);
+
+  const retrievedMessage = JSON.parse(message);
+  console.log("New message: " + retrievedMessage);
+  const Firestore = require("@google-cloud/firestore");
+  // Use your project ID here
+  const PROJECTID = "hotel-management-serverless";
+  const COLLECTION_NAME = "orders";
+  const firestore = new Firestore({
+    projectId: PROJECTID,
+    timestampsInSnapshots: true,
+  });
+
+  return firestore
+    .collection(COLLECTION_NAME)
+    .add(retrievedMessage)
+    .then((doc) => {
+      console.info("stored new doc id#", doc.id);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
