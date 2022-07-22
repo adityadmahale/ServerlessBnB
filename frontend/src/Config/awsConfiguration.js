3f2c6b0f82530c8e20e5a7fd0e98d352692bb7d8
Sourav Malik <sr343164@dal.ca>
2022-07-21 21:09:20 -0300

    Deleted awsConfiguration.js

--- frontend/src/Config/awsConfiguration.js
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
-
-const poolData = {
-    UserPoolId: 'us-east-1_PFnZ1ZKiJ',
-    ClientId: '27aib7mp5e27k77j3eti349pv'
-};
-
-export default new CognitoUserPool(poolData);
